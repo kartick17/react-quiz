3bc2cccccc422684488906b98699d29aa92ef40a
kartick17 <karticksadhu0@gmail.com>
2024-04-25 23:39:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useReducer } from 'react'
+import Header from './Header'
+
+const initialState = {
+  questions: [],
+
+  // "loading","error", "ready", "active", "finished"
+  status: 'loading',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      }
+
+    default:
+      throw new Error('Action unknown')
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  )
+}
